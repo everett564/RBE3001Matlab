efa2c2664f622a2af7782d9f893b919c82a85ac0
Naomi Tivol Harrison <ntharrison@ak120d-05.wpi.edu>
2019-01-19 17:12:48 -0500

    finished all but extra credit

--- src/lab1.m
+++ src/lab1.m
@@ -32,53 +32,98 @@ myHIDSimplePacketComs.connect();
 % Create a PacketProcessor object to send data to the nucleo firmware
 pp = PacketProcessor(myHIDSimplePacketComs); 
 try
-  SERV_ID = 01;            % we will be talking to server ID 37 on
-                           % the Nucleo
-
+  SERV_ID = 01; 
+  SERV_ID_READ = 03;% we will be talking to server ID 37 on
+  SERV_ID_PID = 04;% the Nucleo
+  
+  
+  Kp_Shoulder=.001;
+  Ki_Shoulder=.001;
+  Kd_Shoulder=0.02;
+  
+  
+  Kp_Elbow=.003;
+  Ki_Elbow=.0025;
+  Kd_Elbow=.04;
+  
+  Kp_Wrist=.0006;
+  Ki_Wrist=.0025;
+  Kd_Wrist=.05;
+  
   DEBUG   = true;          % enables/disables debug prints
 
   % Instantiate a packet - the following instruction allocates 64
   % bytes for this purpose. Recall that the HID interface supports
   % packet sizes up to 64 bytes.
   packet = zeros(15, 1, 'single');
-
+  packet(1) = Kp_Shoulder;
+  packet(2) = Ki_Shoulder;
+  packet(3) = Kd_Shoulder;
+  
+  packet(4) = Kp_Elbow;
+  packet(5) = Ki_Elbow;
+  packet(6) = Kd_Elbow;
+  
+  packet(7) = Kp_Wrist;
+  packet(8) = Ki_Wrist;
+  packet(9) = Kd_Wrist;
+  
+  pp.write(SERV_ID_PID,packet);
+  pause(.003);
+  
+  packet = zeros(15, 1, 'single');
+    
   % The following code generates a sinusoidal trajectory to be
   % executed on joint 1 of the arm and iteratively sends the list of
   % setpoints to the Nucleo firmware. 
-  shoulder = [100, 50, 100, -50, 60, 0];
-  elbow = [100, 50, 0, 0, 0, 0];
-  wrist = [0, 0, 0, 0, 0, 0];
+  %shoulder = [0, 324, -18, -404, -295, 0];
+  %elbow = [0, 133.3, -75, -14, 44, 0];
+  %wrist = [0, 221, 386, 79, 129, 0];
+  
+  
+  shoulder = [0, -295, -295, -295,-295, -295];
+  elbow = [0, 444, 444, 444, 444, 444];
+  wrist = [0, 129, 129, 129, 129, 129];
   
 
   ret = [];
 
   % Iterate through a sine wave for joint values
-  for tea = 1:length(shoulder)
-      i = 1;
-      
+  
+  %for tea = 1:length(shoulder)
+  i=1;
+  tea=1;
+  
+  while tea<=6
+     
       tic
       %incremtal = (single(k) / sinWaveInc);
-      packet = zeros(15, 1, 'single');
-      packet(1) = shoulder(tea);
-      packet(2) = elbow(tea);
-      packet(3) = wrist(tea);
+      
+      
       
 
      viaPts = zeros(1, 100);
       % Send packet to the server and get the response
       
       %pp.write sends a 15 float packet to the micro controller
-       pp.write(SERV_ID, packet); 
-       
-       pause(0.003); % Minimum81cd190760243010df8062b3e4106a585b3cff80 amount of time required between write and read
+       if mod(i,166)==0
+        packet = zeros(15, 1, 'single');
+        packet(1) = shoulder(tea);
+        packet(2) = elbow(tea);
+        packet(3) = wrist(tea);
+        tea=tea+1;
+        pp.write(SERV_ID, packet); 
+       end
+       pause(0.003); % Minimum amount of time required between write and read
        
        %pp.read reads a returned 15 float backet from the nucleo.
-       
-       returnPacket = pp.read(SERV_ID);
+       pp.write(SERV_ID_READ, zeros(15,1,'single'));
+       pause(0.003);
+       returnPacket = pp.read(SERV_ID_READ);
        
        ret = [ret;returnPacket'];
        i= i+1;
-       pause(0.003);
+       
       
        
        toc
@@ -97,18 +142,18 @@ try
           packet((x*3)+3)=0;
       end
       %THis version will send the command once per call of pp.write
-      pp.write(02, packet);
-      pause(0.003);
-      returnPacket2=  pp.read(02);
+      %pp.write(02, packet);
+      %pause(0.003);
+      %returnPacket2=  pp.read(02);
       %this version will start an auto-polling server and read back the
       %current data
       %returnPacket2=  pp.command(65, packet);
       if DEBUG
-          disp('Received Packet 2:');
-          disp(returnPacket2);
+          %disp('Received Packet 2:');
+          %disp(returnPacket2);
       end
       toc
-      pause(1) %timeit(returnPacket) !FIXME why is this needed?
+      pause(.003) %timeit(returnPacket) !FIXME why is this needed?
       
   end
 catch exception
@@ -117,12 +162,22 @@ catch exception
 end
 % Clear up memory upon termination
  rep = [];
+ retE = [];
+ retW = [];
+ 
  ret(1,:) = [];
  rep = ret;
- ret = ret(:,1);
+ ret = ret(:,1:3);
+ 
+ 
  plot(ret);
+% plot(retE);
+ %plot(retW);
+ 
  csvwrite('Return File', rep);
- csvwrite('Plot File', ret);
+ csvwrite('Plot File Shoulder', ret);
+ csvwrite('Plot File Elbow', retE);
+ csvwrite('Plot File Wrist', retW);
 
 pp.shutdown()
 
