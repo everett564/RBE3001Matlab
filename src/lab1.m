dbe5308878177b09c8bd51a757a54a377c848cf7
Naomi Tivol Harrison <ntharrison@ak120d-05.wpi.edu>
2019-01-28 13:50:34 -0500

    Final Code for Lab 2

--- src/lab1.m
+++ src/lab1.m
@@ -60,10 +60,9 @@ try
   Ki_Shoulder=.001;
   Kd_Shoulder=0.02;
   
-  
-  Kp_Elbow=.003;
+  Kp_Elbow=.0015;
   Ki_Elbow=.0025;
-  Kd_Elbow=.04;
+  Kd_Elbow=.075;
   
   Kp_Wrist=.0006;
   Ki_Wrist=.0025;
@@ -105,7 +104,40 @@ try
   %wrist = [0, 221, 386, 79, 129, 0];
   
   
-  shoulder = [0, 0,0, 0,0, 0,0,0,0,0];
+  elbowPoly1 = cubePoly(1, 5, 0, 0, 7.55, 55.02);
+  wristPoly1 = cubePoly(1, 5, 0, 0, -254.75, 302.5);
+  
+  elbowPoly2 = cubePoly(5, 9, 0, 0, 55.02, 580);
+  wristPoly2 = cubePoly(5, 9, 0, 0, 302.5, -254.75);
+  
+  elbowPoly3 = cubePoly(9, 13, 0, 0, 580, 7.55);
+  wristPoly3 = cubePoly(9, 13, 0, 0, -254.75, -254.75);
+  
+  elbowPose(1) = 0;
+  wristPose(1) = 0;
+  
+  for i=1:10
+      t = (i-1)*.4 +1;
+      elbowPose(i+1) = polyToPos(elbowPoly1, t);
+      wristPose(i+1) = polyToPos(wristPoly1, t);
+  end
+  
+  for j=1:10
+      t = (j-1)*.4 +5;
+      elbowPose(j+11) = polyToPos(elbowPoly2, t);
+      wristPose(j+11) = polyToPos(wristPoly2, t);
+  end
+      
+  for k=1:10
+      t = (k-1)*.4 +9;
+      elbowPose(k+21) = polyToPos(elbowPoly3, t);
+      wristPose(k+21) = polyToPos(wristPoly3, t);
+  end
+  
+  elbowPose(32) = 0;
+  wristPose(32) = 0;
+  
+  shoulder = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   elbow = [0, 0, 7.55, 55.02, 580, 100, 100,100,100,100];
   wrist = [0, 0, -254.75, 302.5,-254.75, 0,0,0,0,0];
   
@@ -121,23 +153,25 @@ try
   tea=1;
   tic
   timerVal = tic;
-  
-  while tea<=6
+  counter = 0
+  while tea<=32
      
-      
+      counterVal = tic
       %incremtal = (single(k) / sinWaveInc);
 
      viaPts = zeros(1, 100);
       % Send packet to the server and get the response
       
+      
       %pp.write sends a 15 float packet to the micro controller
-       if mod(i,75)==0
+       if counter>=0.4
         packet = zeros(15, 1, 'single');
         packet(1) = shoulder(tea);
-        packet(2) = elbow(tea);
-        packet(3) = wrist(tea);
+        packet(2) = elbowPose(tea);
+        packet(3) = wristPose(tea);
         tea=tea+1;
         pp.write(SERV_ID, packet); 
+        counter = 0
        end
        pause(0.003); % Minimum amount of time required between write and read
        
@@ -160,6 +194,9 @@ try
        ret = [ret;returnPacket(1)];
        ret2 = [ret2;returnPacket(2)];
        ret3 = [ret3;returnPacket(3)];
+       
+       
+       counter = toc(counterVal)+counter
        
        % xAxis = [xAxis;i];
        i= i+1;
@@ -254,7 +291,7 @@ csvwrite('Time', elap);
  
  figure('Name', 'Tip Position', 'NumberTitle', 'off')
  hold on;
- plot(xTip, zTip);
+ plot(xTip, zTip, '-o');
  plot(191.2566,122.9888, 'ro');
  plot(112.3549,-20.2409, 'ro');
  plot(262.5972,5.2783, 'ro');
---
+++ src/polyToPos.m
@@ -0,0 +1,14 @@
+function q = polyToPos(a, t)
+    %takes in 4x1 array of polynomial coefficients a and a given time t
+    %and puts out the target location q
+    
+    %coefficients in input a 
+    a0 = a(1);
+    a1 = a(2);
+    a2 = a(3);
+    a3 = a(4);
+    
+    q = a0 + a1*t + a2*(t^2) + a3*(t^3);
+    q;
+    
+end
