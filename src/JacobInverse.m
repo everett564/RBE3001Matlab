6b60597a594a045723095cdcc41fd81832f655de
Everett R Johnson <erjohnson@ak120d-07.wpi.edu>
2019-02-10 22:01:03 -0500

    So it works, but in the same way that I will get a 100 on the next exam, by pure luck, jk it doesnt work

--- src/JacobInverse.m
+++ src/JacobInverse.m
@@ -1,33 +1,51 @@
 % Jacobian Inverse Function
 % This is step 9 in Lab 4
-% qi is the initial joint positions
-% pd is the final positon
+% qi is the initial joint positions (1x3)
+% pd is the final positon (1x3)
 % returns the final joint angles
 
-function T = JacobInverse(qi,pd)
+function T = JacobInverse(qi, pd)
 
-%Initialization
-q0 = [0,0,0];
+% Makes the Plot
+figure('Name', 'Current Position', 'NumberTitle', 'off') 
+plot3([0,0],[0,0],[0,0]);
+title('Current Postition')
 
-q0 = qi;
+% Sets the View
+view([0,0])
 
-[P1, P2, P3, P4, Z1, Z2, Z3] = fwkinJacob(q0(1),q0(2),q0(3)); 
+% Gets the position of the end effector from the forward kinematics
+[P1, P2, P3, P4, Z1, Z2, Z3] = fwkinJacob(qi(1),qi(2),qi(3)); 
 
-while (pd - (P4')) > 0
-
-% Forward Kinematics 
-[P1, P2, P3, P4, Z1, Z2, Z3] = fwkinJacob(q0(1),q0(2),q0(3));    
+while norm(pd - (P4')) > 0  
+    
+%calculates the position every time it runs through the loop    
+[P1, P2, P3, P4, Z1, Z2, Z3] = fwkinJacob(qi(1),qi(2),qi(3));    
     
 % Gets the Jacobian
-J = jacob0(q0)
-    
-delq = inv(J)*(pd - (P4'));
+J = jacob0(qi);
+A = J(1:3,1:3);
+inJ = inv(A);    
 
-q0 = q0 + delq;
+delq = (pd -(P4'))*inJ;
+
+qi = qi + delq;
+
+[p1, p2, p3, p4, Z1, Z2, Z3] = fwkinJacob(qi(1),qi(2),qi(3));
+    
+% plot3 plots the arm in 3D
+plot3([0, p2(1), p3(1), p4(1)], [0, p2(2), p3(2), p4(2)], [0, p2(3), p3(3), p4(3)],'-or');
+
+xlim([0,350])
+ylim([-200,200])
+zlim([-50,300])
+
+%used to show the iterations it makes in the graph
+pause(.001);
 
 end
 
 % return
-T = q0;
+T = qi;
 
 end
--- src/simulation.m
+++ src/simulation.m
@@ -1,32 +1,25 @@
-clear all
 close all
 
-
-figure('Name', 'Current Position', 'NumberTitle', 'off')
-plot3([0,0],[0,0],[0,0]);
-title('Current Postition')
-xlabel('Position x')
-ylabel('Position y')
-zlabel('Position z')
-view([0,0])
-
+% Initial configuration for the plot
 qi = [0,0,0];
 
 while 1
-    [x,z] = ginput;
     
+    figure(1)
+    xlim([0,350])
+    ylim([0,200]) % this is actually the z value
+
+    % gets the input
+    [x,z] = ginput;
+   
     pd = [x,0,z];
     
-    T = JacobInverse(qi,pd)
+    % Runs this function to modify the graph that is used 
+    T = JacobInverse(qi,pd);
     
-    [p1, p2, p3, p4, Z1, Z2, Z3] = fwkinJacob(T(1),T(2),T(3)); 
+    % Sets the old position as the new start position
+    qi = pd;
     
-    % plot3 plots the arm in 3D
-    plot3([0, p2(1), p3(1), p4(1)], [0, p2(2), p3(2), p4(2)], [0, p2(3), p3(3), p4(3)],'-or');
-    
-    xlim([0,350])
-    ylim([-200,200])
-    zlim([-50,300]) 
-    
-    qi = T;
+    % Displays the Angles from the Jacob Inverse Function
+    disp(T);
 end
